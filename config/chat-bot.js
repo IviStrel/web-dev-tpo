f99cb4ab88a53d0616db021f3675729499d95452
Fernando Diaz <fernydiaz62@gmail.com>
2025-11-14 17:30:26 -0300

    Se agregó la integración con n8n para tener un chatbot y se aplico una configuración global para el mismo

---
+++ config/chat-bot.js
@@ -0,0 +1,22 @@
+export const chatBotConfig = {
+  webhookUrl:
+    "https://prueba.metaldev.cloud/webhook/c916721c-c9b1-4b82-aae8-016c9a4c7591/chat",
+
+  initialMessages: [
+    "¡Hola! 👋",
+    "Mi nombre es Nathan. ¿En qué puedo ayudarte hoy?",
+  ],
+
+  defaultLanguage: "es",
+
+  i18n: {
+    es: {
+      title: "¡Hola! 👋",
+      subtitle:
+        "Inicia un chat. Estamos aquí para ayudarte las 24 horas del día, los 7 días de la semana.",
+      footer: "",
+      getStarted: "Nueva Conversación",
+      inputPlaceholder: "Escriba su pregunta..",
+    },
+  },
+};
